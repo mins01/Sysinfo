a7de82e29dae81a4839d194417441925ceaf8a26
ms.lee <ms.lee@pinspot.co.kr>
2016-07-15 17:52:09 +0900

    first

---
+++ app/src/main/java/com/mins01/sysinfo/InfoActivityFragment.java
@@ -0,0 +1,27 @@
+package com.mins01.sysinfo;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class InfoActivityFragment extends Fragment {
+
+    public InfoActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+
+    public void onclick(View v){
+        Log.e("onclick", String.valueOf(v.getId()));
+    }
+}
